c058e7b6aab8fd134b19d484125ef06b09518631
Dusan-Lex <dusanlexmilosavljevic@gmail.com>
2021-05-28 00:48:08 +0200

    setting up a mongodb database

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,10 +1,19 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const userEmail = req.body.email;
     if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "Invalid email address" });
       return;
     }
+    const client = await MongoClient.connect(
+      "mongodb+srv://Lex:NiKa2019@cluster0.vn6er.mongodb.net/newsletter?retryWrites=true&w=majority",
+      { useUnifiedTopology: true }
+    );
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+    client.close();
     console.log(userEmail);
     res.status(201).json({ message: "Signed up" });
   }
