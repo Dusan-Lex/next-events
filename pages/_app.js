996ae875e47865b0a143425a5d2af0e32e9b3fcd
Dusan-Lex <dusanlexmilosavljevic@gmail.com>
2021-05-28 19:13:01 +0200

    creating and using react context

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import Head from "next/head";
 
 import Layout from "../components/layout/layout";
+import { NotificationContextProvider } from "../store/notification-contex";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
---
+++ store/notification-contex.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+const NotificationContext = createContext({
+  notification: null,
+  showNotification: (notificationData) => {},
+  hideNotification: () => {},
+});
+
+export const NotificationContextProvider = (props) => {
+  const [activeNotification, setActiveNotification] = useState({
+    title: "test",
+    message: "this is test",
+    status: "error",
+  });
+
+  const showNotificationHandler = (notificationData) => {
+    setActiveNotification(notificationData);
+  };
+
+  const hideNotificationHandler = () => {
+    setActiveNotification(null);
+  };
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+};
+export default NotificationContext;
